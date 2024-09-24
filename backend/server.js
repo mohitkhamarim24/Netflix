2e1a474319b6670d39ddf452adc1d4b799e765d9
Mohit Khamari <mohitkhamarim24@gmail.com>
2024-09-24 11:40:16 +0530

    On going Backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import movieRoutes from './routes/movie.routes.js';
 import authRoutes from './routes/auth.route.js';
 import { ENV_VARS } from './config/envVars.js';
 import { connect } from 'mongoose';
@@ -10,7 +11,10 @@ app.all("/", (req, res) => {
     res.send("Welcome to the API");
 });
 app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/movies",movieRoutes);
 app.listen(PORT, () => {
     console.log('Server started at http://localhost:' + PORT); 
     connectDB();
 });
+
+
